74794f0ee9f60347bc5e0337d0f7f3f9e1c49852
rizki-rianto666 <mrizkirianto116@gmail.com>
2025-10-25 07:23:24 +0700

    ADD control pump

--- models/Telemetry.js
+++ models/Telemetry.js
@@ -6,6 +6,9 @@ const TelemetrySchema = new mongoose.Schema(
         ph: { type: Number, min: 0, max: 14, required: true },
         ppm: { type: Number, min: 0, required: true },
         temp: { type: Number, required: true },
+        humidity: { type: Number, required: false },
+        nutritionOn: { type: Boolean, required: false },
+        pesticideOn: { type: Boolean, required: false },
         ts: { type: Date, default: Date.now, index: true },
     },
     { versionKey: false }
